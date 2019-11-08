06cfd3ae7093d4ae719793d9fc811564019b99cd
Vítor Marçal <git@vitormarcal.com.br>
2019-11-08 11:41:33 -0300

    add animação inimigos

--- src/br/com/vitormarcal/entities/Enemy.java
+++ src/br/com/vitormarcal/entities/Enemy.java
@@ -1,16 +1,28 @@
 package br.com.vitormarcal.entities;
 
 import br.com.vitormarcal.main.Game;
+import br.com.vitormarcal.world.Camera;
 import br.com.vitormarcal.world.World;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Enemy extends Entity {
 
-    private double speed = 1;
+    private double speed = 0.8;
+
+    private int frames = 0;
+    private int maxFrames = 20;
+    private int index = 0;
+    private int maxIndex = 1;
+
+    private BufferedImage[] sprites;
 
     public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-        super(x, y, width, height, sprite);
+        super(x, y, width, height, null);
+        sprites = new BufferedImage[2];
+        sprites[0] = Game.spritesheet.getSprite(112, 16, 16, 16);
+        sprites[1] = Game.spritesheet.getSprite(112 + 16, 16, 16, 16);
     }
 
     @Override
@@ -29,5 +41,21 @@ public class Enemy extends Entity {
             }
         }
 
+
+        frames++;
+        if (frames == maxFrames) {
+            frames = 0;
+            index++;
+            if (index > maxIndex) {
+                index = 0;
+            }
+        }
+
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(sprites[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
     }
 }
